a226e8fd9e9457f2140720e092b0147275268f8c
mpannu03 <47941021+mpannu03@users.noreply.github.com>
2024-11-20 11:18:22 +0530

    Updated Comments

--- src/svg/svg.ts
+++ src/svg/svg.ts
@@ -1,5 +1,5 @@
 import { glob } from "glob";
-import path from 'path';
+import path, { resolve } from 'path';
 import sharp from "sharp";
 import fs from 'fs';
 import chalk from "chalk";
@@ -8,7 +8,7 @@ export async function svg2png(inputDir: string, outputDir: string) {
     const svgFiles = await glob(`${inputDir}/**/*.svg`);
     console.log(chalk.green(`Found ${svgFiles.length} SVG file(s) for conversion.`));
 
-    svgFiles.forEach(async (item) => {
+    const conversionPromise = svgFiles.map(async (item) => {
         const outputPath = `${outputDir}\\${path.relative(inputDir, item)}`;
         const doubleSlashPath = outputPath.replace(/\\/g, '\\\\');
 
@@ -16,16 +16,24 @@ export async function svg2png(inputDir: string, outputDir: string) {
             await fs.promises.mkdir(path.dirname(doubleSlashPath), { recursive: true });
         }
 
-        sharp(item)
-            .png()
-            .toFile(outputPath.replace('.svg', '.png'), (err, info) => {
-                if (err) {
-                    console.error(chalk.red(`Error converting file ${item} to PNG:`), err);
-                } else {
-                    process.stdout.write(`Successfully converted ${item} to png\r`);
-                }
-              });
-
+        return new Promise((resolve, reject) => {
+            sharp(item)
+                .png()
+                .toFile(outputPath.replace('.svg', '.png'), (err, info) => {
+                    if (err) {
+                        console.error(chalk.red(`Error converting file ${item} to PNG:`), err);
+                        reject(err);
+                    } else {
+                        process.stdout.write(`Successfully converted ${item} to png\r`);
+                        resolve(info);
+                    }
+                });
+        });
     });
+
+    await Promise.all(conversionPromise);
+    process.stdout.clearLine(0)
+    process.stdout.cursorTo(0)
+    process.stdout.write(`Converted ${svgFiles.length} SVG file(s).\n`);
     console.log(chalk.green('Completed SVG Conversion.'));
 }
